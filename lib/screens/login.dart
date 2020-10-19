5548dc6c5cea521b39da3a27a1ca1db2320b96e1
Connor Gallimore <connor.gallimore@epitech.eu>
2020-10-19 20:40:05 +0200

    Fixed overflow and made TextFields linked

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,77 +14,91 @@ class _LoginScreenState extends State<LoginScreen> {
   String _password;
   String _loginErrorMessage = '';
 
+  void signIn() async {
+    var _state = Provider.of<UserState>(context, listen: false);
+    if (_email != null && _password != null && _email.isNotEmpty && _password.isNotEmpty) {
+      var tmp = await _state.signInWithEmailAndPassword(_email, _password);
+      setState(() {
+        _loginErrorMessage = tmp;
+      });
+      if (_state.isLoggedIn()) Navigator.pushNamed(context, '/');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEFF6EE),
+      resizeToAvoidBottomPadding: false,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/images/aerogrow_logo.jpg'),
-              backgroundColor: Colors.transparent,
-            ),
-            SizedBox(height: 20),
-            TextField(
-              onChanged: (text) {
-                setState(() {
-                  _email = text;
-                });
-              },
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  //floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "Enter your email",
-                  labelText: "Email"),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              onChanged: (text) {
-                setState(() {
-                  _password = text;
-                });
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  //floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "Enter your password",
-                  labelText: 'Password'),
-            ),
-            SizedBox(height: 50),
-            FlatButton(
-              color: Colors.transparent,
-              textColor: Color(0xFF29A19C),
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text(
-                "Sign Up",
-              ),
-            ),
-            FlatButton(
-              color: Color(0xFFA3F7B7),
-              textColor: Color(0xFF393E46),
-              onPressed: () async {
-                var _state = Provider.of<UserState>(context, listen: false);
-                if (_email.isNotEmpty && _password.isNotEmpty) {
-                  var tmp = await _state.signInWithEmailAndPassword(_email, _password);
-                  setState(() {
-                    _loginErrorMessage = tmp;
-                  });
-                  if (_state.isLoggedIn()) Navigator.pushNamed(context, '/');
-                }
-              },
-              child: Text(
-                "Sign In",
-              ),
-            ),
-            Text(
-              _loginErrorMessage,
-              style: kErrorTextStyle,
+            SizedBox(height: 80),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/aerogrow_logo.jpg'),
+                  backgroundColor: Colors.transparent,
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  onChanged: (text) {
+                    setState(() {
+                      _email = text;
+                    });
+                  },
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      //floatingLabelBehavior: FloatingLabelBehavior.always,
+                      hintText: "Enter your email",
+                      labelText: "Email"),
+                  textInputAction: TextInputAction.next,
+                  onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        _password = text;
+                      });
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        //floatingLabelBehavior: FloatingLabelBehavior.always,
+                        hintText: "Enter your password",
+                        labelText: 'Password'),
+                    textInputAction: TextInputAction.done,
+                    onSubmitted: (_) {
+                      FocusScope.of(context).unfocus();
+                      signIn();
+                    }),
+                SizedBox(height: 50),
+                FlatButton(
+                  color: Colors.transparent,
+                  textColor: Color(0xFF29A19C),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: Text(
+                    "Sign Up",
+                  ),
+                ),
+                FlatButton(
+                  color: Color(0xFFA3F7B7),
+                  textColor: Color(0xFF393E46),
+                  onPressed: signIn,
+                  child: Text(
+                    "Sign In",
+                  ),
+                ),
+                Text(
+                  _loginErrorMessage,
+                  style: kErrorTextStyle,
+                ),
+              ],
             ),
           ],
         ),
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -17,6 +17,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   String _registerResult = '';
 
+  void register() async {
+    var _state = Provider.of<UserState>(context, listen: false);
+    if (_mail != null &&
+        _password != null &&
+        _firstName != null &&
+        _lastName != null &&
+        _mail.isNotEmpty &&
+        _password.isNotEmpty &&
+        _firstName.isNotEmpty &&
+        _lastName.isNotEmpty) {
+      var tmp = await _state.registerWithEmailAndPassword(_mail, _password, _firstName, _lastName);
+      setState(() {
+        _registerResult = tmp;
+      });
+      if (_state.isLoggedIn()) Navigator.pushNamed(context, '/');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,76 +49,82 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: SafeArea(
         child: Container(
           margin: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            children: [
-              TextField(
-                onChanged: (text) {
-                  setState(() {
-                    _mail = text;
-                  });
-                },
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    //floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintText: "Enter your e-mail",
-                    labelText: 'Mail'),
+          child: ListView(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        _mail = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        //floatingLabelBehavior: FloatingLabelBehavior.always,
+                        hintText: "Enter your e-mail",
+                        labelText: 'Email'),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        _password = text;
+                      });
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        //floatingLabelBehavior: FloatingLabelBehavior.always,
+                        hintText: "Enter your password",
+                        labelText: 'Password'),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        _firstName = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        //floatingLabelBehavior: FloatingLabelBehavior.always,
+                        hintText: "Enter your first name",
+                        labelText: 'First name'),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        _lastName = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        //floatingLabelBehavior: FloatingLabelBehavior.always,
+                        hintText: "Enter your last name",
+                        labelText: 'Last name'),
+                    textInputAction: TextInputAction.done,
+                    onSubmitted: (_) {
+                      FocusScope.of(context).unfocus();
+                      register();
+                    },
+                  ),
+                  Text(
+                    _registerResult,
+                    style: kErrorTextStyle,
+                  ),
+                  FlatButton(
+                    color: Color(0xFFA3F7B7),
+                    textColor: Color(0xFF393E46),
+                    onPressed: register,
+                    child: Text('Register'),
+                  )
+                ],
               ),
-              TextField(
-                onChanged: (text) {
-                  setState(() {
-                    _password = text;
-                  });
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    //floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintText: "Enter your password",
-                    labelText: 'Password'),
-              ),
-              TextField(
-                onChanged: (text) {
-                  setState(() {
-                    _firstName = text;
-                  });
-                },
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    //floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintText: "Enter your first name",
-                    labelText: 'First name'),
-              ),
-              TextField(
-                onChanged: (text) {
-                  setState(() {
-                    _lastName = text;
-                  });
-                },
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    //floatingLabelBehavior: FloatingLabelBehavior.always,
-                    hintText: "Enter your last name",
-                    labelText: 'Last name'),
-              ),
-              Text(
-                _registerResult,
-                style: kErrorTextStyle,
-              ),
-              FlatButton(
-                color: Color(0xFFA3F7B7),
-                textColor: Color(0xFF393E46),
-                onPressed: () async {
-                  var _state = Provider.of<UserState>(context, listen: false);
-                  if (_mail.isNotEmpty && _password.isNotEmpty && _firstName.isNotEmpty && _lastName.isNotEmpty) {
-                    var tmp = await _state.registerWithEmailAndPassword(_mail, _password, _firstName, _lastName);
-                    setState(() {
-                      _registerResult = tmp;
-                    });
-                    if (_state.isLoggedIn()) Navigator.pushNamed(context, '/');
-                  }
-                },
-                child: Text('Register'),
-              )
             ],
           ),
         ),
