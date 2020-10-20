2fd6b0711a853bd1d710a4ea7dca8b8cfe252032
Connor Gallimore <connor.gallimore@epitech.eu>
2020-10-20 20:16:59 +0200

    Multiple changes:
    Removed unused functions and imports
    Fixed spaceBetween not working

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_instant_messenger/models/conversation_models.dart';
-import 'package:flutter_instant_messenger/services/user_service.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_instant_messenger/widgets/topbar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,18 +7,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Conversation> _convHistory;
-
-  void _signOut() {
-    Provider.of<UserState>(context, listen: false).signOut(context);
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEFF6EE),
-      appBar: TopBar(),
+      appBar: TopBar(context),
       body: SafeArea(
         child: ListView.builder(
           itemCount: 10,
@@ -37,26 +27,26 @@ class _HomeScreenState extends State<HomeScreen> {
               child: InkWell(
                 onTap: null,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
                     ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Icon(
-                              Icons.account_circle_rounded,
-                              size: 100,
-                              color: Color(0xFF29A19C),
-                            ),
-                            Column(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.account_circle_rounded,
+                            size: 100,
+                            color: Color(0xFF29A19C),
+                          ),
+                          Expanded(
+                            child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
                                     Text(
                                       "UserName",
@@ -66,20 +56,25 @@ class _HomeScreenState extends State<HomeScreen> {
                                         color: Color(0xFFEFF6EE),
                                       ),
                                     ),
-                                    Text(
-                                      "18/10/2020",
-                                      style: TextStyle(
-                                        fontFamily: "SourceSansPro",
-                                        fontSize: 16,
-                                        color: Color(0xFFEFF6EE),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 10),
+                                      child: Text(
+                                        "18/10/2020",
+                                        style: TextStyle(
+                                          fontFamily: "SourceSansPro",
+                                          fontSize: 16,
+                                          color: Color(0xFFEFF6EE),
+                                        ),
                                       ),
                                     ),
                                   ],
-                                )
+                                ),
                               ],
-                            )
-                          ],
-                        )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
