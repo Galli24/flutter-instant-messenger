fc30ce92098083746500544ea77a93507f7d23ea
Connor Gallimore <connor.gallimore@epitech.eu>
2020-10-27 23:15:02 +0100

    Fixed the center point not appearing

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -135,7 +135,7 @@ class _ConversationScreenState extends State<ConversationScreen> {
                     Marker(
                       width: 80.0,
                       height: 80.0,
-                      point: LatLng(51.5, -0.09),
+                      point: LatLng(double.parse(pos[0]), double.parse(pos[1])),
                       builder: (ctx) => Icon(Icons.location_on, color: Colors.red, size: 40),
                     ),
                   ],
