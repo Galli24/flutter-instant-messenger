299727a618e862038730aaf64e77874c075d1646
Cantiteau Bastien <bastien.cantiteau@epitech.eu>
2020-10-31 13:07:37 +0100

    Added login widget tests
    tests widget of login screen (not working yet)

---
+++ test/login_widget_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_instant_messenger/screens/login.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(LoginScreen());
+
+    final titleFinder = find.text('AeroGrow');
+    final subtitleFinder = find.text('Messaging App');
+
+    expect(titleFinder, findsOneWidget);
+    expect(subtitleFinder, findsOneWidget);
+
+    //expect(find.text('AeroGrow'), findsOneWidget);
+    //expect(find.text('Messaging App'), findsOneWidget);
+
+    //var textFormField = find.byType(TextFormField);
+    //expect(TextFormField, findsWidgets);
+    //await tester.enterText(textFormField, 'Hello');
+    //expect(find.text('Hello'), findsOneWidget);
+
+    /* 
+    //Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    //Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    */
+  });
+}
